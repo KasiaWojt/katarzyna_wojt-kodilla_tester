4d148e80b7a857fb00ec14f8f6aa6f29ee3f124f
Katarzyna Wójt <katarzynawojt6@gmail.com>
2019-12-05 22:05:21 +0100

    modul 2

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,17 @@
+public class FirstClass {
+    public static void main(String[] args) {
+
+        Notebook notebook = new Notebook(600, 1000, 2019);
+        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
+        notebook.checkPrice();
+
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2018);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + heavyNotebook.year);
+        heavyNotebook.checkWeight();
+
+        Notebook oldNotebook = new Notebook(1600, 500, 2017);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year);
+        oldNotebook.checkYear();
+
+    }
+}
---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,48 @@
+public class Notebook {
+
+    int weight;
+    int price;
+    int year;
+
+    public Notebook(int weight, int price, int year) {
+
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+
+    }
+
+    public void checkPrice() {
+
+        if (price < 500) {
+            System.out.println("This notebook is very cheap");
+        } else if (price > 500 && price < 1000) {
+            System.out.println("This price is good");
+        } else {
+            System.out.println("This notebook is quite expensive");
+        }
+    }
+    public void checkWeight() {
+
+        if (weight < 1600) {
+            System.out.println("This notebook is light");
+        } else if (weight > 1600 && weight < 2000) {
+            System.out.println("This notebook is not too heavy");
+        } else {
+            System.out.println("This notebook is heavy");
+        }
+    }
+    public void checkYear() {
+
+        if (year < 2018 && price < 600) {
+            System.out.println("This notebook is old");
+        } else if (year > 2018 && price < 1000 && year<2019) {
+            System.out.println("This notebook is not too old");
+        } else if (year > 2019 && price < 1500 && year<2019) {
+            System.out.println("This notebook is not too old");
+        } else {
+            System.out.println("This notebook is new");
+        }
+    }
+
+}
